9f3b4c173a6991de5dd7229288a7310141b419f4
Oleg Budaev <t0r1n88@mail.ru>
2020-05-21 17:17:26 +0800

    Версия 0.15 Создан черновой рабочий поток. Реализовано 4 функции parse_full_data_employersдля для получения дополнительной информации по работодателю parse_full_data_vac_hh - для получения полных данных по вакансии parse_driver_license_vac_hh для получения списка водительских прав parse_specializations_vac_hh для получения отрасли и требуемых компетенций

--- training/temp.py
+++ training/temp.py
@@ -1,5 +1,137 @@
 import os
+import requests
+import json
+
+# """
+# Нужные ключи
+# salary['from'] -Нижняя граница вилки оклада
+# salary['to']-Верняя граница вилки оклада
+# salary['gross'] -Признак того что оклад указан до вычета налогов. В случае если не указано - null.
+# experience['name'] требуемый опыт работы
+# schedule['name'] - график работы
+# employment['name']- тип занятости
+# description - описание
+# key_skills - Информация о ключевых навыках, заявленных в вакансии. Список может быть пустым.
+# accept_handicapped - Указание, что вакансия доступна для соискателей с инвалидностью
+# accept_kids - Указание, что вакансия доступна для соискателей от 14 лет
+# specializations - список специализаций
+# specializations['name'] - Название специализации
+# specializations['profarea_name'] - Название профессиональной области, в которую входит специализация
+# driver_license_types - Список требуемых категорий водительских прав. Список может быть пустым.
+# driver_license_types['id'] - Категория водительских прав. Элемент справочника driver_license_types
+#
+#
+# """
+#
+#
+# def parse_driver_license_vac_hh(data):
+#     """
+#     Функция для парсинга водительских прав требуемых в вакансии
+#     :param data: json
+#     :return:
+#     """
+#     if data:
+#         req_driver_license_lst = []
+#         for license in data:
+#             req_driver_license_lst.append(license['id'])
+#         return req_driver_license_lst
+#     return None
+#
+#
+# def parse_specializations_vac_hh(data):
+#     """
+#     Функция для получения полного списка специализаций и профессиональных областей куда относится вакансия.
+#     Проблема в том что требуемые специализации для одной вакансии бывают разные и профессиональные области тоже.
+#     Для последующего использования в датасете нужно привести эти данные к строке и желательно к одному значению
+#      в каждом признаке
+#     :param data: json
+#     :return: кортеж где:
+#      [0]-Наиболее подходящая профобласть
+#     [1] - Список специализаций(компетенций)
+#     """
+#     from collections import Counter
+#     prof_area_lst = []
+#     # имена специализаций относящихся к разным областям могут иногда совпадать. Поэтому используем множество.
+#     spec_set = set()
+#     # Находим самую частую профессиональную область. Выделяем специализации
+#     for prof_area in data:
+#         prof_area_lst.append(prof_area['profarea_name'])
+#         spec_set.add(prof_area['name'])
+#     # Проводим преобразования
+#     # Наиболее часто встречающееся значение
+#     most_common_prof_area = Counter(prof_area_lst).most_common(1)[0][0]
+#     # Создаем список специализаций.
+#     spec_lst = list(spec_set)
+#     return most_common_prof_area, spec_lst
+#
+#
+# def parse_full_data_vac_hh(url):
+#     URL = 'https://api.hh.ru/vacancies/'
+#     response = requests.get(URL + url)
+#     data = response.json()
+#     # Создаем словарь
+#     data_vac_dcit = dict()
+#     if data.get('salary'):
+#         data_vac_dcit['Нижняя_граница_оклада'] = data.get('salary', {}).get('from', None)  # int
+#         data_vac_dcit['Верхняя_граница_оклада'] = data.get('salary', {}).get('to', None)  # int
+#         data_vac_dcit['Оклад_указан_до_вычета_налогов'] = data.get('gross', {}).get('to', None)  # bool
+#     else:
+#         data_vac_dcit['Нижняя_граница_оклада'] = None
+#         data_vac_dcit['Верхняя_граница_оклада'] = None
+#         data_vac_dcit['Оклад_указан_до_вычета_налогов'] = None
+#     data_vac_dcit['Требуемый_опыт_работы'] = data['experience']['name']  # str
+#     data_vac_dcit['График_работы'] = data['schedule']['name']  # str
+#     data_vac_dcit['Тип_занятости'] = data['employment']['name']  # str
+#     data_vac_dcit['Описание_вакансии'] = data['description']  # str + html
+#     data_vac_dcit['Ключевые_навыки'] = data['key_skills']  # list
+#     data_vac_dcit['Вакансия_для_инвалидов'] = data['accept_handicapped']  # bool
+#     data_vac_dcit['Вакансия_для_детей_с_14'] = data['accept_kids']  # bool
+#     data_vac_dcit['Вакансия_для_детей_с_14'] = data['accept_kids']  # bool
+#     data_vac_dcit['Название_профессиональной_области'], \
+#     data_vac_dcit['Список_компетенций'] = parse_specializations_vac_hh(data['specializations'])  # str, list
+#     data_vac_dcit['Требуемые_водительские_права'] = parse_driver_license_vac_hh(
+#         data['driver_license_types'])  # list or None
+#
+#     return data_vac_dcit
+#
+#
+# # id_vac = '20388682'
+# id_vac = '36150517'
+# vac_data = parse_full_data_vac_hh(id_vac)
+# print(vac_data)
+#
+# # vac_data = requests.get('https://api.hh.ru/vacancies/36150517').json()
+#
+# with open('vac_1.json', 'w', encoding='utf-8') as f:
+#     json.dump(vac_data, f, sort_keys=False, indent=4, ensure_ascii=False)
+
+"""
 
 
-print(os.path.dirname(__file__))
-print(os.path.join(os.path.dirname(__file__),'lindy.json'))
+"""
+
+
+def parse_full_data_employers(url):
+    """
+    Функция для получения дополнительных данных о работодателе
+    :param url: id работодателя
+    :return: dict с данными
+    """
+    URL = 'https://api.hh.ru/employers/'
+    response = requests.get(URL + url)
+    f = response.json()
+    # Парсим полученный json
+    data_dict = {}
+    if f.get('name', None):
+        data_dict['Название_компании'] = f['name']
+        data_dict['Месторасположение'] = f['area']['name']
+        data_dict['Отрасли_компании '] = [ind['name'] for ind in f['industries']]
+        return data_dict
+    else:
+        return {}
+
+
+id_emp = '6'
+data = parse_full_data_employers(id_emp)
+with open('employer.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, sort_keys=False, indent=4, ensure_ascii=False)
--- training/training_hh.py
+++ training/training_hh.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 import os
 import csv
+import time
 
 
 def check_access_api_hh(url):
@@ -47,6 +48,7 @@ def parse_data_hh(data, url, param_cycle):
     # Парсим полученный json
     # в data['pages] хранится количество страниц соответствующих запросу( на каждой странице по 20 результатов)
     for i in range(0, data['pages']):
+    # for i in range(0, 1):
         param_cycle['page'] = i
         # Отпраляем запрос
         response_cycle = requests.get(url, param_cycle)
@@ -56,24 +58,127 @@ def parse_data_hh(data, url, param_cycle):
         result = result['items']
         # Парсим исходный list формата Json в dictionary (словарь данных)
         for y in range(0, len(result) - 1):
+        # for y in range(0, 2):
+            # Используем 2 функции для получения дополнительных данных
+            # По вакансиям
+            advanced_vac_dict = parse_full_data_vac_hh(result[y]['id'])
+            # по работодателю
+            advanced_employer_dict = parse_full_data_employers(result[y]['employer']['id'])
             dict_data[dict_number] = {
                 'id': result[y]['id'],
                 'premium': result[y]['premium'],
                 'Наименование вакансии': result[y]['name'],
                 'Город': result[y]['area']['name'],
-                # 'Оклад': result[y]['salary'],
-                # 'Минимальный оклад': result[y].get(['salary']['from']),
-                # 'Максимальный оклад': result[y]['salary']['to'],
-                # 'Оклад указан до вычета налогов': result[y]['salary']['gross'],
-                'Дата опубликования вакансии': result[y]['published_at'],
+                'Дата_опубликования_вакансии': result[y]['published_at'],
                 'type_name': result[y]['type']['name'],
                 'Требования': result[y]['snippet']['requirement'],
                 'Обязанности': result[y]['snippet']['responsibility'],
-                'Наименование работодателя': result[y]['employer']['name'],
                 'Ссылка на вакансию': result[y]['alternate_url']
             }
+            dict_data[dict_number].update(advanced_vac_dict)
+            dict_data[dict_number].update(advanced_employer_dict)
+
+
+
             dict_number = dict_number + 1
+        time.sleep(5)
     return dict_data
+
+
+def parse_specializations_vac_hh(data):
+    """
+    Функция для получения полного списка специализаций и профессиональных областей куда относится вакансия.
+    Проблема в том что требуемые специализации для одной вакансии бывают разные и профессиональные области тоже.
+    Для последующего использования в датасете нужно привести эти данные к строке и желательно к одному значению
+     в каждом признаке
+    :param data: json
+    :return: кортеж где:
+     [0]-Наиболее подходящая профобласть
+    [1] - Список специализаций(компетенций)
+    """
+    from collections import Counter
+    prof_area_lst = []
+    # имена специализаций относящихся к разным областям могут иногда совпадать. Поэтому используем множество.
+    spec_set = set()
+    # Находим самую частую профессиональную область. Выделяем специализации
+    for prof_area in data:
+        prof_area_lst.append(prof_area['profarea_name'])
+        spec_set.add(prof_area['name'])
+    # Проводим преобразования
+    # Наиболее часто встречающееся значение
+    most_common_prof_area = Counter(prof_area_lst).most_common(1)[0][0]
+    # Создаем список специализаций.
+    spec_lst = list(spec_set)
+    return most_common_prof_area, spec_lst
+
+
+def parse_driver_license_vac_hh(data):
+    """
+    Функция для парсинга водительских прав требуемых в вакансии
+    :param data: json
+    :return:
+    """
+    if data:
+        req_driver_license_lst = []
+        for license in data:
+            req_driver_license_lst.append(license['id'])
+        return req_driver_license_lst
+    return None
+
+
+def parse_full_data_vac_hh(url):
+    """
+    Функция для парсинга дополнительных данных вакансии
+    :param url: ссылка на вакансию
+    :return: словарь с нужными данными
+    """
+    URL = 'https://api.hh.ru/vacancies/'
+    response = requests.get(URL + url)
+    data = response.json()
+    # Создаем словарь
+    data_vac_dcit = dict()
+    if data.get('salary'):
+        data_vac_dcit['Нижняя_граница_оклада'] = data.get('salary', {}).get('from', None)  # int
+        data_vac_dcit['Верхняя_граница_оклада'] = data.get('salary', {}).get('to', None)  # int
+        data_vac_dcit['Оклад_указан_до_вычета_налогов'] = data.get('gross', {}).get('to', None)  # bool
+    else:
+        data_vac_dcit['Нижняя_граница_оклада'] = None
+        data_vac_dcit['Верхняя_граница_оклада'] = None
+        data_vac_dcit['Оклад_указан_до_вычета_налогов'] = None
+    data_vac_dcit['Требуемый_опыт_работы'] = data['experience']['name']  # str
+    data_vac_dcit['График_работы'] = data['schedule']['name']  # str
+    data_vac_dcit['Тип_занятости'] = data['employment']['name']  # str
+    data_vac_dcit['Описание_вакансии'] = data['description']  # str + html
+    data_vac_dcit['Ключевые_навыки'] = data['key_skills']  # list
+    data_vac_dcit['Вакансия_для_инвалидов'] = data['accept_handicapped']  # bool
+    data_vac_dcit['Вакансия_для_детей_с_14'] = data['accept_kids']  # bool
+    data_vac_dcit['Вакансия_для_детей_с_14'] = data['accept_kids']  # bool
+    data_vac_dcit['Название_профессиональной_области'], \
+    data_vac_dcit['Список_компетенций'] = parse_specializations_vac_hh(data['specializations'])  # str, list
+    data_vac_dcit['Требуемые_водительские_права'] = parse_driver_license_vac_hh(
+        data['driver_license_types'])  # list or None
+
+    return data_vac_dcit
+
+
+def parse_full_data_employers(url):
+    """
+    Функция для получения дополнительных данных о работодателе
+    :param url: id работодателя
+    :return: dict с данными
+    """
+    URL = 'https://api.hh.ru/employers/'
+    response = requests.get(URL + url)
+    f = response.json()
+    # Парсим полученный json
+    data_dict = {}
+    if f.get('name', None):
+        data_dict['Название_компании'] = f['name'] # str
+        data_dict['Месторасположение'] = f['area']['name'] # str
+        data_dict['Отрасли_компании '] = [ind['name'] for ind in f['industries']] # list
+        return data_dict
+    else:
+        return {}
 
 
 def save_file_to_json(data):
@@ -121,5 +226,7 @@ if __name__ == '__main__':
     data = load_data_hh(URL, param)  # Загружаем сырые данные с сайта
     assert type(data) == dict
     parsed_data = parse_data_hh(data, URL, param)  # Обрабатываем сырые данные
-    path_to_csv_hh = save_file_to_csv(parsed_data)
+    print(parsed_data)
+    para= save_file_to_json(parsed_data)
+    # path_to_csv_hh = save_file_to_csv(parsed_data)
 
